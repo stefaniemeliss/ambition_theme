ecb7ee5ad5c1a6dfd1f0f184564b9bf993673522
Stefanie Meliss, PhD <55539825+stefaniemeliss@users.noreply.github.com>
2024-09-10 17:38:46 +0100

    Update ambition_theme.R

--- ambition_theme.R
+++ ambition_theme.R
@@ -50,10 +50,10 @@ font_size = 12
 head_size = font_size + 2
 
 # define theme for plot
-ambition_theme <- theme_bw(base_family = "SegoeUI") + 
+ambition_theme <- theme_bw(base_family = "Segoe UI") + 
   theme(
     # font size and face settings
-    text = element_text(family = "SegoeUI", size = font_size),
+    text = element_text(family = "Segoe UI", size = font_size),
     title = element_text(face = "bold", size = font_size),
     plot.title = element_text(size = head_size, face = "bold"),
     plot.subtitle = element_text(size = font_size, face = "plain"),
